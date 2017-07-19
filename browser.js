4149d71d21735eb70ad925ca62180fa1693d1372
MLaszczewski <m8@em8.pl>
2017-07-19 18:08:01 +0200

    observable list in browser.js

--- browser.js
+++ browser.js
@@ -3,11 +3,14 @@ var rd = ReactiveDao
 
 import Observable from "./lib/Observable.js"
 import ObservableValue from "./lib/ObservableValue.js"
+import ObservableList from "./lib/ObservableList.js"
 
 rd.Observable = ObservableValue
 export { Observable }
 rd.ObservableValue = ObservableValue
 export { ObservableValue }
+rd.ObservableList = ObservableList
+export { ObservableList }
 
 import ReactiveCache from "./lib/ReactiveCache.js"
 rd.ReactiveCache = ReactiveCache
